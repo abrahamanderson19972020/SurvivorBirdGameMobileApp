fbe3a182c06d5c9eff24fec4545f54a905cc8e77
abrahamanderson19972020 <abraham.anderson83@gmail.com>
2025-08-14 21:13:34 +0200

    4.versions

---
+++ core/src/main/kotlin/com/abraham/survivorbirdgame/screens/BirdEscapeScreen.kt
@@ -0,0 +1,34 @@
+package com.abraham.survivorbirdgame.screens
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Screen
+
+class BirdEscapeScreen(private val game: Game):Screen {
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+
+    override fun show() {
+        TODO("Not yet implemented")
+    }
+
+    override fun render(delta: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun resize(width: Int, height: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun pause() {
+        TODO("Not yet implemented")
+    }
+
+    override fun resume() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hide() {
+        TODO("Not yet implemented")
+    }
+}
--- core/src/main/kotlin/com/abraham/survivorbirdgame/screens/HomeScreen.kt
+++ core/src/main/kotlin/com/abraham/survivorbirdgame/screens/HomeScreen.kt
@@ -13,13 +13,17 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Rectangle
-import com.badlogic.gdx.math.Vector2
 
 class HomeScreen(private val game: Game): Screen {
 
     private lateinit var batch: SpriteBatch
     private lateinit var font: BitmapFont
     private lateinit var shapeRenderer: ShapeRenderer
+    private lateinit var titleFont:BitmapFont
+    private lateinit var background:Texture
+    private lateinit var cardBackground:Texture
+    private lateinit var leftArrow:Texture
+    private lateinit var rightArrow:Texture
 
     // List of carousel cards
     private val cards = mutableListOf<GameCard>()
@@ -28,6 +32,11 @@ class HomeScreen(private val game: Game): Screen {
     private var scrollX = 0f
     private var startTouchX = 0f
     private var isDragging = false
+
+    private val cardWidth = 500f
+    private val cardHeight = 400f
+    private val cardGap = 50f
+    private var maxScroll = 0f
 
     data class GameCard(
         val title: String,
@@ -40,24 +49,25 @@ class HomeScreen(private val game: Game): Screen {
         batch = SpriteBatch()
         font = BitmapFont()
         font.data.setScale(2f)
+        titleFont = BitmapFont()
+        titleFont.data.setScale(3f)
+
         shapeRenderer = ShapeRenderer()
 
-        // Add your game cards here
-        cards.add(GameCard(
-            "Math Master",
-            "Solve fun math challenges!",
-            Texture("images/math_game.png"),
-            { MathCategoryScreen(game) }
-        ))
+        background = Texture("images/home_bg.png")
+        cardBackground = Texture("images/card_bg.png")
+        leftArrow = Texture("images/arrow_left.png")
+        rightArrow = Texture("images/arrow_right.png")
 
-        cards.add(GameCard(
-            "Catch Kenny",
-            "Catch Kenny before he escapes!",
-            Texture("images/catch_kenny.jpeg"),
-            { CatchKennyScreen(game) }
-        ))
+        cards.add(GameCard("Math Master", "Solve fun math challenges!", Texture("images/math_game.png")) { MathCategoryScreen(game) })
+        cards.add(GameCard("Catch Kenny", "Catch Kenny before he escapes!", Texture("images/catch_kenny.jpeg")) { CatchKennyScreen(game) })
+        cards.add(GameCard("Bird Escape", "Dodge and survive!", Texture("images/bird_escape.png")) { BirdEscapeScreen(game) })
+        cards.add(GameCard("Survivor Mode", "Endless flying challenge!", Texture("images/survivor_mode.png")) { SurvivorModeScreen(game) })
+        cards.add(GameCard("Memory Match", "Test your memory!", Texture("images/memory_match.png")) { MemoryMatchScreen(game) })
+        cards.add(GameCard("Puzzle Quest", "Solve tricky puzzles!", Texture("images/puzzle_quest.jpg")) { PuzzleQuestScreen(game) })
 
-        // More games can be added here
+        maxScroll = ((cards.size * (cardWidth + cardGap)) - cardGap) - Gdx.graphics.width
+        if (maxScroll < 0) maxScroll = 0f
     }
 
     override fun render(delta: Float) {
@@ -72,45 +82,33 @@ class HomeScreen(private val game: Game): Screen {
         val centerX = Gdx.graphics.width / 2f
         val centerY = Gdx.graphics.height / 2f
 
-        // Draw cards
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
-        cards.forEachIndexed { index, card ->
-            val x = centerX - cardWidth / 2 + (index * (cardWidth + cardGap)) - scrollX
-            val y = centerY - cardHeight / 2
-
-            // Only draw cards visible on screen for performance
-            if (x + cardWidth > 0 && x < Gdx.graphics.width) {
-                shapeRenderer.color = Color.WHITE
-                shapeRenderer.rect(x, y, cardWidth, cardHeight)
-            }
-        }
-        shapeRenderer.end()
-
-        // Draw card images and text
         batch.begin()
+        batch.draw(background, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+
+        // Draw cards
         cards.forEachIndexed { index, card ->
-            val x = centerX - cardWidth / 2 + (index * (cardWidth + cardGap)) - scrollX
+            val x = index * (cardWidth + cardGap) - scrollX // CHANGE: left-aligned carousel
             val y = centerY - cardHeight / 2
 
             if (x + cardWidth > 0 && x < Gdx.graphics.width) {
-                // Draw game image
-                batch.draw(card.image, x + 20f, y + cardHeight / 3f, cardWidth - 40f, cardHeight / 2f)
-
-                // Draw title
-                val titleLayout = GlyphLayout(font, card.title)
-                font.color = Color.BLACK
-                font.draw(batch, titleLayout, x + (cardWidth - titleLayout.width) / 2f, y + cardHeight - 40f)
-
-                // Draw description
-                val descLayout = GlyphLayout(font, card.description)
-                font.draw(batch, descLayout, x + (cardWidth - descLayout.width) / 2f, y + 40f)
+                batch.draw(card.image, x, y, cardWidth, cardHeight)
             }
         }
+        // Navigation arrows - ADDED
+        val centerYArrow = Gdx.graphics.height / 2f - 50f
+        batch.draw(leftArrow, 50f, centerY - 50f, 100f, 100f)
+        batch.draw(rightArrow, Gdx.graphics.width - 150f, centerY - 50f, 100f, 100f)
+
         batch.end()
     }
 
     private fun handleInput() {
         // Drag to scroll carousel
+        val cardWidth = 500f
+        val cardGap = 50f
+        val maxScroll = (cards.size * (cardWidth + cardGap)) - cardWidth // ADDED
+        scrollX = scrollX.coerceIn(0f, maxScroll) // ADDED
+
         if (Gdx.input.isTouched) {
             val touchX = Gdx.input.x.toFloat()
             if (!isDragging) {
@@ -119,6 +117,7 @@ class HomeScreen(private val game: Game): Screen {
             } else {
                 val delta = startTouchX - touchX
                 scrollX += delta
+                scrollX = scrollX.coerceIn(0f, maxScroll)
                 startTouchX = touchX
             }
         } else {
@@ -130,17 +129,33 @@ class HomeScreen(private val game: Game): Screen {
             val touchX = Gdx.input.x.toFloat()
             val touchY = Gdx.graphics.height - Gdx.input.y.toFloat()
 
-            val cardWidth = 500f
-            val cardHeight = 400f
-            val cardGap = 50f
-            val centerX = Gdx.graphics.width / 2f
-            val centerY = Gdx.graphics.height / 2f
+            // Left arrow scroll
+            if (Rectangle(50f, Gdx.graphics.height / 2f - 50f, 100f, 100f).contains(
+                    touchX,
+                    touchY
+                )
+            ) {
+                scrollX -= cardWidth + cardGap // CHANGE: scroll left one card
+            }
 
+            // Right arrow scroll
+            if (Rectangle(
+                    Gdx.graphics.width - 150f,
+                    Gdx.graphics.height / 2f - 50f,
+                    100f,
+                    100f
+                ).contains(touchX, touchY)
+            ) {
+                scrollX += cardWidth + cardGap // CHANGE: scroll right one card
+            }
+
+            scrollX = scrollX.coerceIn(0f, maxScroll) // CHANGE: clamp scrollX
+
+            // CHANGE: tap on card to select
             cards.forEachIndexed { index, card ->
-                val x = centerX - cardWidth / 2 + (index * (cardWidth + cardGap)) - scrollX
-                val y = centerY - cardHeight / 2
-                val rect = Rectangle(x, y, cardWidth, cardHeight)
-                if (rect.contains(touchX, touchY)) {
+                val x = index * (cardWidth + cardGap) - scrollX
+                val y = Gdx.graphics.height / 2f - cardHeight / 2
+                if (Rectangle(x, y, cardWidth, cardHeight).contains(touchX, touchY)) {
                     game.screen = card.screenFactory()
                 }
             }
@@ -155,6 +170,10 @@ class HomeScreen(private val game: Game): Screen {
         batch.dispose()
         font.dispose()
         shapeRenderer.dispose()
+        background.dispose()
+        cardBackground.dispose()
+        leftArrow.dispose()
+        rightArrow.dispose()
         cards.forEach { it.image.dispose() }
     }
 }
